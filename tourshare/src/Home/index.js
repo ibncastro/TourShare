cd98f206fe81dab44bf6d236144eebac72869e48
Ridge Agyei <ibncastro20@gmail.com>
2021-10-29 21:59:19 +0000

    Home - header almost complete
    - installed icons
    - set background

--- tourshare/src/Home/index.js
+++ tourshare/src/Home/index.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 export default function Index() {
     return (
-        <div className="bg-header-bg min-h-1/2 bg-gray-100">
-            <h1>This is content</h1>
+        <div className="bg-header-bg flex min-h-1/2 bg-cover py-20 bg-gray-100">
+            <div className="m-auto w-1/4 py-10 text-center">
+            <h1 className="text-7xl text-shadow text-shadow-lg font-extrabold text-white text-shadow-lg">Your Next Trip In The Making </h1>
+            <h3 className="py-2 px-5 text-white">Travelistly is a curated collection of the very best travel stories and documentaries from around the world.</h3>
+            <button className="bg-blue-500 text-white py-2 mt-2 shadow-lg rounded-lg font-semibold px-6">Sign Up</button>
+            </div>
+            
         </div>
     )
 }
--- tourshare/tailwind.config.js
+++ tourshare/tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
         '3/4': '75%',
         'full': '100%',
        },
+       textShadow: { // defaults to {}
+        'default': '0 2px 5px rgba(0, 0, 0, 0.5)',
+        'lg': '0 2px 10px rgba(0, 0, 0, 0.5)',
+      },
     },
     variants: {
       extend: {},
